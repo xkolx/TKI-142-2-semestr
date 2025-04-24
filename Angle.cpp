f07e381b58360e44e4aa3526a4facfdb7dcdaf82
Shlaev Konstantin <57225083+xkolx@users.noreply.github.com>
2025-04-24 16:04:04 +0300

    Update Angle.cpp

--- Angle.cpp
+++ Angle.cpp
@@ -21,20 +21,10 @@ void Angle::normalize() {
     int extraMinutes = seconds / 60;
     minutes += extraMinutes;
     seconds = fmod(seconds, 60.0);
-    
-    if (seconds < 0) {
-        seconds += 60;
-        minutes--;
-    }
 
     int extraDegrees = minutes / 60;
     degrees += extraDegrees;
     minutes = minutes % 60;
-    
-    if (minutes < 0) {
-        minutes += 60;
-        degrees--;
-    }
 }
 
 int Angle::getDegrees() const { return degrees; }
